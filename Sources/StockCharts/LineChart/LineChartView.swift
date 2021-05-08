ae71f25e1e4479c35c588dc931871a9c272aad36
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-05-08 18:38:35 +0200

    Hide ChartLabel on dragGesture

--- Sources/StockCharts/LineChart/LineChartView.swift
+++ Sources/StockCharts/LineChart/LineChartView.swift
@@ -25,9 +25,11 @@ public struct LineChartView: View {
     
     public var body: some View {
         VStack {
-//            ChartLabel(data: data, dates: dates, hours: hours, indexPosition: $indexPosition)
-//                .opacity(showingIndicators ? 1: 0)
-//                .padding(.vertical)
+            if dragGesture ?? true {
+                ChartLabel(data: data, dates: dates, hours: hours, indexPosition: $indexPosition)
+                    .opacity(showingIndicators ? 1: 0)
+                    .padding(.vertical)
+            }
 
             LineView(data: data, dates: dates, hours: hours, dragGesture: dragGesture, showingIndicators: $showingIndicators, indexPosition: $indexPosition)
         }
