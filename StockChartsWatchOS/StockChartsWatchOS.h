37980cb0aa6e818037ec33cf2c430b0604d3f3ed
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-08-05 11:07:06 +0100

    Added WatchOS Target

---
+++ StockChartsWatchOS/StockChartsWatchOS.h
@@ -0,0 +1,18 @@
+//
+//  StockChartsWatchOS.h
+//  StockChartsWatchOS
+//
+//  Created by Dennis Concepción Martín on 5/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StockChartsWatchOS.
+FOUNDATION_EXPORT double StockChartsWatchOSVersionNumber;
+
+//! Project version string for StockChartsWatchOS.
+FOUNDATION_EXPORT const unsigned char StockChartsWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StockChartsWatchOS/PublicHeader.h>
+
+
